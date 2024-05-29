de106a97e9849993fa675e9306fc76db45faa2e6
megbalf <megbalf@uw.edu>
2024-05-29 10:33:30 -0700

    Search Function

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Card(props) {
     const cardAttributes = props.attributes.map((attribute, index) => (
@@ -22,18 +22,32 @@ function Card(props) {
 
 export function Explore(props) {
 
-    let sortedData = props.data;
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const studySpotCards = sortedData.map((spot, index) => (
-        <Card key={index} title={spot.title} link={spot.link} image={spot.image} attributes={spot.attributes} />
-        ))
+  const handleSearchChange = (event) => {
+      setSearchQuery(event.target.value);
+    };
+  
+    // Filter the data based on the search query
+   const filteredData = props.data.filter(spot => 
+      spot.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      spot.attributes.some(attr => attr.toLowerCase().includes(searchQuery.toLowerCase()))
+    );
+  
+  const studySpotCards = filteredData.map((spot, index) => (
+    <Card key={index} title={spot.title} link={spot.link} image={spot.image} attributes={spot.attributes} />
+  ));
+
+    // const studySpotCards = sortedData.map((spot, index) => (
+    //     <Card key={index} title={spot.title} link={spot.link} image={spot.image} attributes={spot.attributes} />
+    //     ))
 
     return (
         <div className="index-container">
             <h1>Look at some of our top study spots on campus!</h1>
             <div className="search-bar">
                 <label htmlFor="filter">Search:</label>
-                <input type="text" id="filter" />
+                <input type="text" id="filter" value={searchQuery} onChange={handleSearchChange} />
             </div>
     
             <div className="card-container">
